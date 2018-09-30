21da4ba1d28e7abec77f128756b951cdf78d5823
AndresGarciadelaHuerta <agarciadelahuerta2@uc.cl>
2018-09-30 19:48:43 -0300

    add_time

--- main_simulacion.py
+++ main_simulacion.py
@@ -1,5 +1,6 @@
 import numpy
 import simulacion
+import time
 
 a = True
 while a:
@@ -15,6 +16,7 @@ while a:
         print('Por favor ingrese un número entero.')
 
 # Leemos excel y poblamos
+tiempo1 = time.time()
 estaciones = simulacion.poblar()
 s = simulacion.Simulador()
 s.estaciones = estaciones
@@ -23,6 +25,7 @@ s.prints = False
 
 lista_porcentajes = []
 i = 0
+tiempo2 = time.time()
 while i < numero_simulaciones:
 
     print('\nCorriendo repetición {}.'.format(str(i + 1)))
@@ -56,10 +59,11 @@ while i < numero_simulaciones:
 
     estaciones_dda_insatisfecha.sort()
     estaciones_dda_satisfecha.sort()
-    print('\nLas 3 estaciones con mayor cantidad de satisfaccion de demanda :')
-    print(estaciones_dda_satisfecha[-3:])
-    print('\nLas 3 estaciones con mayor cantidad de insatisfaccion de demanda :')
-    print(estaciones_dda_insatisfecha[-3:])
+    if s.prints:
+        print('\nLas 3 estaciones con mayor cantidad de satisfaccion de demanda :')
+        print(estaciones_dda_satisfecha[-3:])
+        print('\nLas 3 estaciones con mayor cantidad de insatisfaccion de demanda :')
+        print(estaciones_dda_insatisfecha[-3:])
 
 
 
@@ -70,12 +74,16 @@ while i < numero_simulaciones:
     #print("Porcentaje de Satisfaccion de la Demanda: " + str(
      #   porcentaje_satisfaccion) + "%")
 
-
+tiempo3 = time.time()
 promedio_satisfaccion = sum(lista_porcentajes)/len(lista_porcentajes)
 varianza = round((float(numpy.std(lista_porcentajes).item())**2), 4)
-print(lista_porcentajes)
+#print(lista_porcentajes)
 print('--------------------------------------------------------------')
 print('Porcentaje Promedio de Satisfaccion de la Demanda: ' + str(
         promedio_satisfaccion) + "%")
 print('Varianza de los Porcentajes de Satisfacción de la Demanda: ' + str(
     varianza))
+print('Tiempo en leer los datos: ' + str(round(tiempo2 - tiempo1, 2))
+      + ' segundos.')
+print('Tiempo en simular todas las repeticiones: ' + str(round(
+    tiempo3 - tiempo2, 2)) + ' segundos.')
