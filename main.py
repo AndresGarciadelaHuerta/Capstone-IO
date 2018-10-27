47abb77a83c25de8bae64d1a9e483123a9937635
Vicente Ramos <vicho@MacBook-Pro-de-Vicente.local>
2018-10-26 21:44:39 -0300

    heuristica

--- main.py
+++ main.py
@@ -5,93 +5,164 @@ from scipy.stats import t
 import numpy
 
 if __name__ == '__main__':
-    a = True
-    while a:
+    # lista_aux = []
+    # dist = open('hola.csv', 'r')
+    # for i in dist:
+    #     lista_aux.append(int(i))
 
-        try:
-            numero_simulaciones = int(
-                input('Ingrese el número de repeticiones de la '
-                      'simulacion que quiere realizar '
-                      '(debe ser un número entero): '))
-            a = False
+    lista_aux = [27, 15, 27, 19, 4, 6, 26, 21, 4, 21, 22, 27, 15, 23, 25, 9, 13, 20, 4, 15, 4, 26, 2, 25, 10, 27, 25, 6,
+                 12, 9, 24, 11, 10, 41, 42, 31, 4, 5, 13, 1, 10, 22, 26, 36, 29, 29, 19, 46, 23, 6, 11, 37, 45, 6, 5,
+                 11, 12, 36, 0, 19, 33, 2, 5, 5, 11, 26, 19, 50, 19, 25, 3, 7, 21, 0, 26, 10, 6, 3, 35, 9, 39, 42, 21,
+                 15, 4, 13, 8, 14, 40, 10, 4, 29]
 
-        except ValueError:
-            print('Por favor ingrese un número entero.')
+    print(lista_aux)
+
+    intervalo_bajo = 0
 
     # Poblamos
-    tiempo1 = time.time()
-    estaciones = read_json()
-    s = simulacion.Simulador()
-    s.estaciones = estaciones
-    s.prints = False
 
-    lista_porcentajes = []
-    i = 0
-    tiempo2 = time.time()
-    while i < numero_simulaciones:
+    while intervalo_bajo < 80:
+        tiempo1 = time.time()
+        estaciones = read_json()
+        s = simulacion.Simulador()
+        s.lista_aux = lista_aux
+        s.estaciones = estaciones
+        s.prints = False
 
-        print('\nCorriendo repetición {}.'.format(str(i + 1)))
+        lista_porcentajes = []
+        i = 0
+        numero_simulaciones = 0
+        intervalo_alto = 999999999
+        intervalo_bajo = 0
+        tiempo2 = time.time()
 
-        # Reseteamos
-        s.tiempo_actual = 0
-        s.cola = []
-        s.contador_dias = 0
-        s.demanda_insatisfecha = 0
-        s.demanda_satisfecha = 0
+        # Reajuste por sobra/necesidad
+        # demandas_por_estacion = {i: 0 for i in range(1, 93)}
 
-        # Resetear cada estacion
-        for estacion in s.estaciones.values():
-            estacion.demanda_satisfecha = 0
-            estacion.demanda_insatisfecha = 0
+        # Reajuste por satisfaccion
+        demandas_por_estacion = {
+            i: {j: 0 for j in ('satisfechos', 'insatisfechos', 'manana', 'mediodia', 'tarde', 'noche')} for i in
+            range(1, 93)}
 
-        # Corremos la simulación, los clusters y el ruteo
-        s.run()
-        clusters = opti_final(estaciones)
-        for grupo in clusters.values():
-            ruteo(grupo, s.estaciones)
+        while (intervalo_alto - intervalo_bajo) > 2 or numero_simulaciones < 15:
+            numero_simulaciones += 1
 
-        # Obtenemos las medidas de desempeño
+            # print('\nCorriendo repetición {}.'.format(str(numero_simulaciones)))
+            # print(intervalo_alto - intervalo_bajo, numero_simulaciones, '\n')
+            # print(intervalo_bajo, intervalo_alto)
 
-        porcentaje_satisfaccion = round(
-            (s.demanda_satisfecha / (s.demanda_insatisfecha +
-                                     s.demanda_satisfecha)) * 100, 2)
-        lista_porcentajes.append(porcentaje_satisfaccion)
+            # Reseteamos
+            s.tiempo_actual = 0
+            s.cola = []
+            s.contador_dias = 0
+            s.demanda_insatisfecha = 0
+            s.demanda_satisfecha = 0
 
-        estaciones_dda_satisfecha = [(e.demanda_satisfecha, e.number) for e in
-                                     s.estaciones.values()]
-        estaciones_dda_insatisfecha = [(e.demanda_insatisfecha, e.number) for e in
-                                       s.estaciones.values()]
+            # Resetear cada estacion
+            for estacion in s.estaciones.values():
+                estacion.demanda_satisfecha = 0
+                estacion.demanda_insatisfecha = 0
 
-        estaciones_dda_insatisfecha.sort()
-        estaciones_dda_satisfecha.sort()
-        if s.prints:
-            print('\nLas 3 estaciones con mayor cantidad de satisfaccion de demanda :')
-            print(estaciones_dda_satisfecha[-3:])
-            print('\nLas 3 estaciones con mayor cantidad de insatisfaccion de demanda :')
-            print(estaciones_dda_insatisfecha[-3:])
+            # Corremos la simulación, los clusters y el ruteo
+            s.run()
+            if False:
+                clusters = opti_final(estaciones)
+                for grupo in clusters.values():
+                    ruteo(grupo, s.estaciones)
 
-        i += 1
-        # print("Porcentaje de Satisfaccion de la Demanda: " + str(
-        #   porcentaje_satisfaccion) + "%")
+            # Obtenemos las medidas de desempeño
 
-    tiempo3 = time.time()
-    promedio_satisfaccion = sum(lista_porcentajes) / len(lista_porcentajes)
-    varianza = round((float(numpy.std(lista_porcentajes).item()) ** 2), 4)
+            porcentaje_satisfaccion = round(
+                (s.demanda_satisfecha / (s.demanda_insatisfecha +
+                                         s.demanda_satisfecha)) * 100, 2)
+            lista_porcentajes.append(porcentaje_satisfaccion)
 
-    # Intervalo de confianza al 95%
+            estaciones_dda_satisfecha = [(e.demanda_satisfecha, e.number) for e in
+                                         s.estaciones.values()]
+            estaciones_dda_insatisfecha = [(e.demanda_insatisfecha, e.number) for e in
+                                           s.estaciones.values()]
 
-    studiante = t.interval(.95, numero_simulaciones - 1)
-    intervalo_bajo = promedio_satisfaccion + studiante[0] * sqrt(varianza) / sqrt(numero_simulaciones)
-    intervalo_alto = promedio_satisfaccion + studiante[1] * sqrt(varianza) / sqrt(numero_simulaciones)
+            # Reajuste por satisfaccion
+            for estacion in s.estaciones.values():
+                demandas_por_estacion[estacion.num]['satisfechos'] += estacion.demanda_satisfecha
+                demandas_por_estacion[estacion.num]['insatisfechos'] += estacion.demanda_insatisfecha
+                demandas_por_estacion[estacion.num]['manana'] += estacion.demanda_insatisfecha_manana
+                demandas_por_estacion[estacion.num]['mediodia'] += estacion.demanda_insatisfecha_mediodia
+                demandas_por_estacion[estacion.num]['tarde'] += estacion.demanda_insatisfecha_tarde
+                demandas_por_estacion[estacion.num]['noche'] += estacion.demanda_insatisfecha_noche
 
-    # print(lista_porcentajes)
-    print('--------------------------------------------------------------')
-    print('Porcentaje Promedio de Satisfaccion de la Demanda: ' + str(
-        promedio_satisfaccion) + "%")
-    print('Varianza de los Porcentajes de Satisfacción de la Demanda: ' + str(
-        varianza))
-    print('Intervalo de confianza al 95% de satisfacción: {} <= X <= {}'.format(intervalo_bajo, intervalo_alto))
-    print('Tiempo en leer los datos: ' + str(round(tiempo2 - tiempo1, 2))
-          + ' segundos.')
-    print('Tiempo en simular todas las repeticiones: ' + str(round(
-        tiempo3 - tiempo2, 2)) + ' segundos.')
+            # Reajuste por sobras/necesidad
+            # for estacion in s.estaciones.values():
+            #     demandas_por_estacion[estacion.num] += estacion.inventario - estacion.inv_manana
+
+            promedio_satisfaccion = sum(lista_porcentajes) / len(lista_porcentajes)
+            varianza = round((float(numpy.std(lista_porcentajes).item()) ** 2), 4)
+
+            # Intervalo de confianza al 95%
+
+            studiante = t.interval(.95, numero_simulaciones - 1)
+            intervalo_bajo = promedio_satisfaccion + studiante[0] * sqrt(varianza) / sqrt(numero_simulaciones)
+            intervalo_alto = promedio_satisfaccion + studiante[1] * sqrt(varianza) / sqrt(numero_simulaciones)
+
+        tiempo3 = time.time()
+        promedio_satisfaccion = sum(lista_porcentajes) / len(lista_porcentajes)
+        varianza = round((float(numpy.std(lista_porcentajes).item()) ** 2), 4)
+
+        # Intervalo de confianza al 95%
+
+        studiante = t.interval(.95, numero_simulaciones - 1)
+        intervalo_bajo = promedio_satisfaccion + studiante[0] * sqrt(varianza) / sqrt(numero_simulaciones)
+        intervalo_alto = promedio_satisfaccion + studiante[1] * sqrt(varianza) / sqrt(numero_simulaciones)
+
+        # reajuste por satisfaccion
+        demandas_estacion_ordenada = sorted(demandas_por_estacion,
+                                            key=lambda est: -(demandas_por_estacion[est]['manana'] * .7 +
+                                                              demandas_por_estacion[est]['mediodia'] * .3))
+
+        # Reajuste por sobras/necesidad
+        # demandas_estacion_ordenada = sorted(demandas_por_estacion, key=lambda est: demandas_por_estacion[est])
+
+        if True:
+            print('\nLas 5 estaciones con mayor cantidad de satisfaccion de demanda :')
+            print(demandas_estacion_ordenada[-5:])
+            print('\nLas 5 estaciones con mayor cantidad de insatisfaccion de demanda :')
+            print(demandas_estacion_ordenada[:5])
+        #
+        # book = open('satisfacciones.csv', 'w')
+        #
+        # book.write('Estacion, inventario, manana, mediodia, tarde, noche\n')
+        #
+        # for est in estaciones.keys():
+        #     num = estaciones[est].num
+        #     book.write('{}, {}, {}, {}, {}, {}\n'.format(num, lista_aux[num - 1], demandas_por_estacion[num]['manana'],
+        #                                                  demandas_por_estacion[num]['mediodia'],
+        #                                                  demandas_por_estacion[num]['tarde'],
+        #                                                  demandas_por_estacion[num]['noche']))
+        #
+        # book.close()
+
+        if intervalo_bajo < 80:
+            numero_de_mayores = 2
+            cambios = 1
+            aum = demandas_estacion_ordenada[:numero_de_mayores]
+            dis = demandas_estacion_ordenada[-numero_de_mayores:]
+            for i in range(numero_de_mayores):
+                if lista_aux[dis[i] - 1] - cambios < 0:
+                    lista_aux[aum[i] - 1] += lista_aux[dis[i] - 1]
+                    lista_aux[dis[i] - 1] = 0
+                else:
+                    lista_aux[aum[i] - 1] += cambios
+                    lista_aux[dis[i] - 1] -= cambios
+            print(lista_aux)
+
+        # print(lista_porcentajes)
+        print('--------------------------------------------------------------')
+        print('Porcentaje Promedio de Satisfaccion de la Demanda: ' + str(
+            promedio_satisfaccion) + "%")
+        print('Varianza de los Porcentajes de Satisfacción de la Demanda: ' + str(
+            varianza))
+        print('Intervalo de confianza al 95% de satisfacción: {} <= X <= {}'.format(intervalo_bajo, intervalo_alto))
+        print('Tiempo en leer los datos: ' + str(round(tiempo2 - tiempo1, 2))
+              + ' segundos.')
+        print('Tiempo en simular todas las repeticiones: ' + str(round(
+            tiempo3 - tiempo2, 2)) + ' segundos.')
--- simulacion.py
+++ simulacion.py
@@ -4,8 +4,6 @@ import numpy
 from math import sqrt
 
 
-
-
 class Simulador:
 
     def __init__(self):
@@ -18,22 +16,22 @@ class Simulador:
         self.demanda_insatisfecha = 0
         self.demanda_satisfecha = 0
         self.prints = False
+        self.lista_aux = None
 
-    def definir_distribucion_manana(self, lista=[]):
-        i = 0
+    def definir_distribucion_manana(self):
         for estacion in self.estaciones.keys():
-            if i % 2 == 0:
-                self.estaciones[estacion].inventario = 17
-                i += 1
-            else:
-                self.estaciones[estacion].inventario = 18
-                i += 1
+            self.estaciones[estacion].inventario = self.lista_aux[self.estaciones[estacion].num - 1]
+            self.estaciones[estacion].inv_manana = self.lista_aux[self.estaciones[estacion].num - 1]
+            self.estaciones[estacion].demanda_insatisfecha_manana = 0
+            self.estaciones[estacion].demanda_insatisfecha_mediodia = 0
+            self.estaciones[estacion].demanda_insatisfecha_tarde = 0
+            self.estaciones[estacion].demanda_insatisfecha_noche = 0
 
     def llegadas_personas_manana(self):
 
         for estacion in self.estaciones.keys():
             self.estaciones[estacion].proxima_llegada = int((8 * 60) + expovariate(
-                float(1/self.estaciones[estacion].tasa_manana)))
+                float(1 / self.estaciones[estacion].tasa_manana)))
             self.cola.append((int(self.estaciones[estacion].proxima_llegada),
                               "Inicio persona", estacion))
             self.cola.sort()
@@ -41,13 +39,12 @@ class Simulador:
     def tiempo_viaje_persona(self, estacion1, estacion2):
         a = [float(self.estaciones[estacion1].x), float(self.estaciones[estacion1].y)]
         b = [float(self.estaciones[estacion2].x), float(self.estaciones[estacion2].y)]
-        #distancia = numpy.linalg.norm(a-b)
+        # distancia = numpy.linalg.norm(a-b)
         # Esta en Kms/Hora
-        distancia = float(sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2))
-        tiempo_en_llegar = int((distancia/self.velocidad_bicicleta) * 60)
+        distancia = float(sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2))
+        tiempo_en_llegar = int((distancia / self.velocidad_bicicleta) * 60)
         return (self.tiempo_actual + tiempo_en_llegar, "Fin persona",
-                                                                    estacion2)
-
+                estacion2)
 
     def run(self):
         """
@@ -66,9 +63,9 @@ class Simulador:
             evento = self.cola[0]
             self.tiempo_actual = evento[0]
 
-            #if self.tiempo_actual > 1200:
-             #   self.contador_dias = 1
-              #  return
+            # if self.tiempo_actual > 1200:
+            #   self.contador_dias = 1
+            #  return
 
             if evento[1] == "Inicio persona":
                 if self.estaciones[evento[2]].inventario == 0:
@@ -80,71 +77,74 @@ class Simulador:
                     if self.tiempo_actual < (11 * 60):
                         self.estaciones[evento[2]].proxima_llegada_manana()
                         tiempo_nueva_llegada = int(self.tiempo_actual + \
-                                               self.estaciones[
-                                                   evento[2]].proxima_llegada)
+                                                   self.estaciones[
+                                                       evento[2]].proxima_llegada)
+                        self.estaciones[evento[2]].demanda_insatisfecha_manana += 1
                     elif self.tiempo_actual < (14 * 60):
                         self.estaciones[evento[2]].proxima_llegada_mediodia()
                         tiempo_nueva_llegada = int(self.tiempo_actual + \
-                                               self.estaciones[
-                                                   evento[2]].proxima_llegada)
+                                                   self.estaciones[
+                                                       evento[2]].proxima_llegada)
+                        self.estaciones[evento[2]].demanda_insatisfecha_mediodia += 1
                     elif self.tiempo_actual < (17 * 60):
                         self.estaciones[evento[2]].proxima_llegada_tarde()
                         tiempo_nueva_llegada = int(self.tiempo_actual + \
-                                               self.estaciones[
-                                                   evento[2]].proxima_llegada)
+                                                   self.estaciones[
+                                                       evento[2]].proxima_llegada)
+                        self.estaciones[evento[2]].demanda_insatisfecha_tarde += 1
                     else:
                         self.estaciones[evento[2]].proxima_llegada_noche()
                         tiempo_nueva_llegada = int(self.tiempo_actual + \
-                                               self.estaciones[
-                                                   evento[2]].proxima_llegada)
+                                                   self.estaciones[
+                                                       evento[2]].proxima_llegada)
+                        self.estaciones[evento[2]].demanda_insatisfecha_noche += 1
                 elif self.estaciones[evento[2]].inventario > 0:
                     if self.prints:
                         print(str(evento) + "Demanda Satisfecha")
                     if self.tiempo_actual < (11 * 60):
                         estacion_llegada = numpy.random.choice(
                             list(self.estaciones.keys()), 1, p=list(self.estaciones[
-                                evento[2]].diccionario_manana.values()))
+                                                                        evento[2]].diccionario_manana.values()))
                         self.estaciones[evento[2]].proxima_llegada_manana()
                         tiempo_nueva_llegada = int(self.tiempo_actual + \
-                                               self.estaciones[
-                                                   evento[2]].proxima_llegada)
+                                                   self.estaciones[
+                                                       evento[2]].proxima_llegada)
                     elif self.tiempo_actual < (14 * 60):
                         estacion_llegada = numpy.random.choice(
                             list(self.estaciones.keys()), 1, p=list(self.estaciones[
-                                evento[2]].diccionario_mediodia.values()))
+                                                                        evento[2]].diccionario_mediodia.values()))
                         self.estaciones[evento[2]].proxima_llegada_mediodia()
                         tiempo_nueva_llegada = int(self.tiempo_actual + \
-                                               self.estaciones[
-                                                   evento[2]].proxima_llegada)
+                                                   self.estaciones[
+                                                       evento[2]].proxima_llegada)
                     elif self.tiempo_actual < (17 * 60):
                         estacion_llegada = numpy.random.choice(
                             list(self.estaciones.keys()), 1, p=list(self.estaciones[
-                                evento[2]].diccionario_tarde.values()))
+                                                                        evento[2]].diccionario_tarde.values()))
                         self.estaciones[evento[2]].proxima_llegada_tarde()
                         tiempo_nueva_llegada = int(self.tiempo_actual + \
-                                               self.estaciones[
-                                                   evento[2]].proxima_llegada)
-                    #Ahora el caso en que self.tiempo_actual < (20 * 60)
+                                                   self.estaciones[
+                                                       evento[2]].proxima_llegada)
+                    # Ahora el caso en que self.tiempo_actual < (20 * 60)
                     ##### CAMBIAR DE TARDE A NOCHE CUANDO ESTE LISTO
                     else:
                         estacion_llegada = numpy.random.choice(
                             list(self.estaciones.keys()), 1, p=list(self.estaciones[
-                                evento[2]].diccionario_noche.values()))
+                                                                        evento[2]].diccionario_noche.values()))
                         self.estaciones[evento[2]].proxima_llegada_noche()
                         tiempo_nueva_llegada = int(self.tiempo_actual + \
-                                               self.estaciones[
-                                                   evento[2]].proxima_llegada)
+                                                   self.estaciones[
+                                                       evento[2]].proxima_llegada)
 
                     self.estaciones[evento[2]].inventario -= 1
                     self.estaciones[evento[2]].demanda_satisfecha += 1
                     self.demanda_satisfecha += 1
-                    #print(estacion_llegada)
+                    # print(estacion_llegada)
                     tupla = self.tiempo_viaje_persona(evento[2],
                                                       str(estacion_llegada[0]))
                     self.cola.pop(0)
                     self.cola.append(tupla)
                     self.cola.sort()
-
 
                 # Definimos nueva llegada
                 if self.tiempo_actual < 1200 and tiempo_nueva_llegada <= 1200:
@@ -160,11 +160,9 @@ class Simulador:
                 self.estaciones[evento[2]].inventario += 1
                 self.cola.pop(0)
 
-
-
-#estaciones = poblar()
-#s = Simulador()
-#s.estaciones = estaciones
-#s.prints = True
-#s.run()
-#print(s.demanda_satisfecha/(s.demanda_insatisfecha + s.demanda_satisfecha))
+# estaciones = poblar()
+# s = Simulador()
+# s.estaciones = estaciones
+# s.prints = True
+# s.run()
+# print(s.demanda_satisfecha/(s.demanda_insatisfecha + s.demanda_satisfecha))
