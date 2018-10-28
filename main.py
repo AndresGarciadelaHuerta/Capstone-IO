dc20b392ac3e644ba5ace0a9fe761ad527889c5c
AndresGarciadelaHuerta <agarciadelahuerta2@uc.cl>
2018-10-28 18:53:11 -0300

    probando flujos

--- main.py
+++ main.py
@@ -5,12 +5,73 @@ from scipy.stats import t
 import numpy
 import csv
 
+lista = []
+contador_menores = 0
+def distribucion_inicial_estacion(estacion, estaciones):
+    estacion.probas(estaciones)
+    llegan_manana = sum(estacion.probs['manana'].values()) * .8
+    #print(llegan_manana)
+    llegan_mediodia = sum(estacion.probs['mediodia'].values()) * .8
+    llegan_tarde = sum(estacion.probs['tarde'].values()) * .8
+    llegan_noche = sum(estacion.probs['noche'].values()) * .8
+    salen_manana = 3 * estacion.tasa_manana * .8
+    #print(salen_manana)
+    salen_mediodia = 3 * estacion.tasa_mediodia * .8
+    salen_tarde = 3 * estacion.tasa_tarde * .8
+    salen_noche = 3 * estacion.tasa_noche * .8
+    inicial = 0
+    manana = llegan_manana - salen_manana
+    mediodia = llegan_mediodia - salen_mediodia
+    tarde = llegan_tarde - salen_tarde
+    noche = llegan_noche - salen_noche
+    suma_ida = manana + mediodia
+    suma_vuelta = tarde + noche
+    suma = manana + mediodia + tarde + noche
+    # if manana < 0:
+    #     inicial += manana
+    # #print(inicial)
+    # if mediodia < 0:
+    #     inicial += mediodia
+    # if tarde < 0:
+    #     inicial += tarde
+    # if noche < 0:
+    #     inicial += noche
+    # #print(inicial)
+    # #print('-------------')
+    # if suma < 0:
+    #     inicial = int(round(suma * -1, 0)) + 40
+    # else:
+    #     if suma < 5:
+    #         inicial = 25
+    #     if suma > 5:
+    #         inicial = 20
+    #     if suma > 10:
+    #         inicial = 15
+    #     if suma > 15:
+    #         inicial = 15
+    #     if suma > 20:
+    #         inicial = 17
+    if suma_ida > 0 and suma_vuelta > 0:
+        inicial = int(round(((suma_ida + suma_vuelta)), 0))
+    elif suma_ida > 0 and suma_vuelta < 0:
+        inicial = int(round((-suma_vuelta-suma_ida * -1)/4, 0))
+    elif suma_ida < 0 and suma_vuelta > 0:
+        inicial = int(round(suma_ida * -1, 0)) + 10
+    elif suma_ida < 0 and suma_vuelta < 0:
+        inicial = int(round((suma_ida + suma_vuelta) * -1, 0))
+
+
+    return inicial
+
+
+
+
 if __name__ == '__main__':
-    lista_aux = []
-    with open('hola.csv', 'r', encoding='utf-8') as file:
-        csv_reader = csv.reader(file, delimiter=',')
-        for i in csv_reader:
-            lista_aux.append(int(i[0].strip('\ufeff')))
+    #lista_aux = []
+    #with open('hola.csv', 'r', encoding='utf-8') as file:
+     #   csv_reader = csv.reader(file, delimiter=',')
+      #  for i in csv_reader:
+       #     lista_aux.append(int(i[0].strip('\ufeff')))
 
     # lista_aux = [27, 15, 27, 19, 4, 6, 26, 21, 4, 21, 22, 27, 15, 23, 25, 9, 13, 20, 4, 15, 4, 26, 2, 25, 10, 27, 25, 6,
     #              12, 9, 24, 11, 10, 41, 42, 31, 4, 5, 13, 1, 10, 22, 26, 36, 29, 29, 19, 46, 23, 6, 11, 37, 45, 6, 5,
@@ -22,8 +83,6 @@ if __name__ == '__main__':
     #              9, 36, 0, 17, 36, 2, 4, 5, 11, 27, 19, 52, 19, 27, 3, 7, 21, 0, 27, 10, 6, 3, 37, 9, 39, 40, 22, 15, 4,
     #              13, 8, 13, 40, 9, 4, 32]
 
-    print(lista_aux)
-    print(sum(lista_aux))
 
     intervalo_bajo = 0
 
@@ -39,9 +98,16 @@ if __name__ == '__main__':
     s = simulacion.Simulador()
 
     if intervalo_bajo < 80:
+        lista_2 = []
         tiempo1 = time.time()
-        s.lista_aux = lista_aux
         s.estaciones = estaciones
+        for estacion in s.estaciones.values():
+            lista_2.append(distribucion_inicial_estacion(estacion, s.estaciones))
+        print(lista_2)
+        print(sum(lista_2))
+        lista_aux = lista_2
+        s.lista_aux = lista_aux
+
         s.prints = False
         lista_porcentajes = []
         i = 0
@@ -58,7 +124,7 @@ if __name__ == '__main__':
             i: {j: 0 for j in ('satisfechos', 'insatisfechos', 'manana', 'mediodia', 'tarde', 'noche')} for i in
             range(1, 93)}
 
-        while (intervalo_alto - intervalo_bajo) > 2 or numero_simulaciones < 10:
+        while (intervalo_alto - intervalo_bajo) > 2 or numero_simulaciones < 5:
             numero_simulaciones += 1
 
             # print('\nCorriendo repetición {}.'.format(str(numero_simulaciones)))
@@ -130,8 +196,7 @@ if __name__ == '__main__':
 
         # reajuste por satisfaccion
         demandas_estacion_ordenada = sorted(demandas_por_estacion,
-                                            key=lambda est: -(demandas_por_estacion[est]['manana'] * .7 +
-                                                              demandas_por_estacion[est]['mediodia'] * .3))
+                                            key=lambda est: (demandas_por_estacion[est]['insatisfechos']))
 
         # Reajuste por sobras/necesidad
         # demandas_estacion_ordenada = sorted(demandas_por_estacion, key=lambda est: demandas_por_estacion[est])
