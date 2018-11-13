4d7cc985a4ff518f50ba80838f2810a0743e855c
achadwicka <achadwick@uc.cl>
2018-11-13 19:37:30 -0300

    Multi subtours

--- Modelo_buti.py
+++ Modelo_buti.py
@@ -98,7 +98,7 @@ def ruteo(grupo, estaciones, prints=True):
     if prints:
         if a != False:
             # con subtour
-            if len(a) > 0:
+            if len(a) > 4:
                 graficar_ruteo(grupo, estaciones, m, c, 0)
                 # sin subtour
                 graficar_ruteo(grupo, estaciones, m, c, a)
@@ -133,11 +133,13 @@ def ruteo(grupo, estaciones, prints=True):
 
 def graficar_ruteo(grupo, estaciones, m, c, con):
     Grafo = nx.DiGraph()
+    #costotot = 0
     for estacion in grupo:
         pos = (float(estaciones['Estación {}'.format(estacion)].x), float(estaciones['Estación {}'.format(estacion)].y))
         Grafo.add_node(estacion, pos=pos)
     Grafo.add_node(0, pos=(0.0, 0.0))
     labels_pencils = {}
+    print('llegue1')
     for var in m.getVars():
         if con == 0:
             if 'y' in var.varName and var.x > 0:
@@ -157,6 +159,7 @@ def graficar_ruteo(grupo, estaciones, m, c, con):
                 j = int(lista[2])
                 labels_pencils[i, j] = '{}'.format(var.x)
         else:
+            print('llegue2')
             if var.varName in con:
                 print('editando')
                 if con[var.varName] > 0:
@@ -165,6 +168,7 @@ def graficar_ruteo(grupo, estaciones, m, c, con):
                     j = int(lista[2])
                     Grafo.add_edge(i, j, cap=q)
                     Grafo[i][j]['cost'] = round(c[i][j], 2)
+                    # costotot += round(c[i][j], 2)
 
             elif 'y' in var.varName and var.x > 0 and var.varName not in \
                     con:
@@ -173,10 +177,13 @@ def graficar_ruteo(grupo, estaciones, m, c, con):
                 j = int(lista[2])
                 Grafo.add_edge(i, j, cap=q)
                 Grafo[i][j]['cost'] = round(c[i][j], 2)
+                #costotot += round(c[i][j], 2)
                 if '_0' in var.varName:
                     for variable in m.getVars():
                         if 'x_{}_{}'.format(i, j) in variable.varName:
                             labels_pencils[i, j] = 'home'
+            else:
+                print('llegue3')
 
             if 'x' in var.varName and var.x > 0:
                 lista = var.varName.split('_')
@@ -190,4 +197,5 @@ def graficar_ruteo(grupo, estaciones, m, c, con):
 
     nx.draw_networkx_edge_labels(Grafo, pos, edge_labels=labels_pencils)
     plt.show()
+    #return costotot
     return m.objVal
